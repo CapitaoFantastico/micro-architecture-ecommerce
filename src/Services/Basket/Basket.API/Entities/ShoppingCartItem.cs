0903651ce31ccee86da48c28d6afc1f1615a3027
Henrique Souza <henriquesouza182@gmail.com>
2021-08-19 06:32:04 -0300

    Solving warnings non nullable fields

--- src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -2,6 +2,15 @@ namespace Basket.API.Entities
 {
     public class ShoppingCartItem
     {
+        public ShoppingCartItem(int quantity, string color, decimal price, string productId, string productName)
+        {
+            Quantity = quantity;
+            Color = color;
+            Price = price;
+            ProductId = productId;
+            ProductName = productName;
+        }
+
         public int Quantity { get; set; }
         public string Color { get; set; }
         public decimal Price { get; set; }
